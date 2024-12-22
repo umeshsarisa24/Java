b263c34bbc3c35349115226af14e3eab09c18e1d
umeshsarisa24 <umeshsarisa24@gmail.com>
2024-12-22 16:21:20 +0530

    adding a file

---
+++ PasswordGenerator.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+public class PasswordGenerator {
+
+    public static String generatePassword(int length) {
+
+        String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/";
+
+
+        String allChars = upperCase + lowerCase + numbers + specialChars;
+
+        StringBuilder password = new StringBuilder();
+
+        Random random = new Random();
+
+        password.append(upperCase.charAt(random.nextInt(upperCase.length())));
+        password.append(lowerCase.charAt(random.nextInt(lowerCase.length())));
+        password.append(numbers.charAt(random.nextInt(numbers.length())));
+        password.append(specialChars.charAt(random.nextInt(specialChars.length())));
+
+        for (int i = 4; i < length; i++) {
+            password.append(allChars.charAt(random.nextInt(allChars.length())));
+        }
+
+        return shuffleString(password.toString());
+    }
+
+    private static String shuffleString(String input) {
+        char[] characters = input.toCharArray();
+        Random random = new Random();
+        for (int i = characters.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            char temp = characters[i];
+            characters[i] = characters[j];
+            characters[j] = temp;
+        }
+        return new String(characters);
+    }
+
+    public static void main(String[] args) {
+        int passwordLength = 12;
+        String password = generatePassword(passwordLength);
+        System.out.println("Generated Password: " + password);
+    }
+}
